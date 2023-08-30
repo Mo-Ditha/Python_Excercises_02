ded9b9f50ffe7103147434695f64d00477b6ab6b
Moditha Dissanayaka <108186528+Mo-Ditha@users.noreply.github.com>
2023-08-30 17:03:37 +0530

    Create pd_ex02.py

---
+++ pd_ex02.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+labels = ['a','b','c']
+my_data = [10,20,30]
+arr = np.array(my_data)
+d = {'a':10,'b':20,'c':30}
+
+print ("Labels:", labels)
+print("My data:", my_data)
+print("Dictionary:", d)
